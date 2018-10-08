4fbf3f2985b5012ef6fc74c6e2e3669359ea51a1
Kody <kihnat@gmail.com>
2018-10-08 09:36:12 -0500

    Basic Create
    Basic Create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import {SuiModule} from 'ng2-semantic-ui';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ManageEmployeesComponent } from './manage-employees/manage-employees.component';
+import { ViewHistoryComponent } from './view-history/view-history.component';
+import { UpcomingComponent } from './upcoming/upcoming.component';
 
+
+const appRoutes: Routes = [
+  { path: '', component: UpcomingComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ManageEmployeesComponent,
+    ViewHistoryComponent,
+    UpcomingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SuiModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {useHash: true})
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
